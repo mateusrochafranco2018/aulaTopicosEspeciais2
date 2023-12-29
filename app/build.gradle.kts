c96bd299a9096c103c826a038e5999ac96456a3e
mateusrochafranco2018 <mateusrochafranco@gmail.com>
2023-12-29 20:55:45 -0300

    atualização

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.aulatopicosespeciais2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.aulatopicosespeciais2"
@@ -30,6 +30,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
